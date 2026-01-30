495bcdff692c873f6aa16558d56fdcc9d23a9674
TobleroneBill <billseaton2.0@gmail.com>
2026-01-30 17:05:56 +0000

    Prototype Scene
    Protoype Features + File restucture

--- skater-boi/TESTINIT.gd
+++ skater-boi/TESTINIT.gd
@@ -4,4 +4,9 @@ func printtest():
 	print('hi')
 
 func _ready():
+	$CSGCylinder3D/AnimationPlayer.play("Rotate")
 	printtest()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	$CSGCylinder3D/AnimationPlayer.play("Rotate")
