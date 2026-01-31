13fa6bdb30e96aed1b3c008e0405fefe00edf412
TobleroneBill <billseaton2.0@gmail.com>
2026-01-31 16:14:10 +0000

    added various assets & animations

--- skater-boi/Scripts/Global.gd
+++ skater-boi/Scripts/Global.gd
@@ -1,3 +1,12 @@
 extends Node
 
 var GameManager : GameManager
+
+
+var type_sounds: Array[AudioStream] = [
+	preload("res://Audio/Sounds/Clicks/Click1.wav"),
+	preload("res://Audio/Sounds/Clicks/Click2.wav"),
+	preload("res://Audio/Sounds/Clicks/Click3.wav"),
+	preload("res://Audio/Sounds/Clicks/Click4.wav"),
+	
+]
