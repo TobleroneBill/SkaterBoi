934ae8a91ccc8b34fbae580989e2e37fabc5a68a
TobleroneBill <billseaton2.0@gmail.com>
2026-01-31 20:23:59 +0000

    Added Visual Timeout Progressbar

--- skater-boi/Game Objects/S-Typer.gd
+++ skater-boi/Game Objects/S-Typer.gd
@@ -2,12 +2,18 @@ extends Sprite3D
 
 @onready var basetext = $"SubViewport/Control/PanelContainer/PanelContainer/Base Text"
 @onready var usertext = $"SubViewport/Control/PanelContainer/PanelContainer/User Text"
+@onready var percentageTime = $SubViewport/Control/PanelContainer/ProgressBar
+
 
 # Set the new string to type out
 func Update(bg,fg=""):
 	#print('input Text:' + fg)
 	basetext.text = bg
 	usertext.text = fg
+
+func setProgress(percentage):
+	#print(percentage)
+	percentageTime.value = percentage
 
 # Animation Functions
 func Fadeout():
